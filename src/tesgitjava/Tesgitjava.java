c224aa7402b8eacff14d67cfc3ecb72c463fd5d5
uwiga-diah <josi.cewek@gmail.com>
2014-01-25 19:48:47 -0800

    Update Tesgitjava.java

--- src/tesgitjava/Tesgitjava.java
+++ src/tesgitjava/Tesgitjava.java
@@ -17,6 +17,7 @@ public class Tesgitjava {
         
         // TODO code application logic here
         system.out println {"tulisan pertama"}; 
+         system.out println {"lanjutkan"}; 
     }
     
 }
